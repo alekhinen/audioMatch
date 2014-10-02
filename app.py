2fbf713e0238c2c0bea7651a82bbf17157cc1333
Nick Alekhine <nick.alekhine@gmail.com>
2014-10-02 11:05:13 -0400

    super ghetto modularization/refactor of assignment4.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 import argparse
-from dan_v2 import compareFiles
+from comparator import parseAndCompare
+from validator import validateFiles
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file1', help='include a file')
 parser.add_argument('file2', help='include a second file')
 args = parser.parse_args()
 
-compareFiles( args.file1, args.file2 )
+if ( validateFiles( args.file1, args.file2 )):
+  parseAndCompare( args.file1, args.file2 )
 
 
 # Shell script so that we can do this: dan -f z01.wav -f z02.wav
