e87d023589b7bb56db4b6a25dcf7eb6fef7e7ce2
Nick Alekhine <nick.alekhine@gmail.com>
2014-10-04 16:14:31 -0400

    set threshold for FFT. understand what FFT actually does. yeah.
    comment out pyplot graph.
    
    clean up. get ready for final version.

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+from scipy.ndimage.filters import maximum_filter
 import scipy.io.wavfile as wavfile
 from scipy.fftpack import fft
 
@@ -6,18 +6,18 @@ def parseFiles(f1, f2):
   samp_rate1, data1 = wavfile.read( f1 )
   samp_rate2, data2 = wavfile.read( f2 )
 
-  ### data1 ####
-  a = data1.T[0] # two channel soundtrack; get the first track
-  b = [ (e/2**8)*2-1 for e in a] # this is 8-bit track, b is now normalized on [-1,1)
-  c = fft(b) # create a list of complex number
-  d = len(c) / 2    # you only need half of the fft list
-  # plt.plot(abs(c[:(d-1)]),'r')
-  # plt.show()
+  # d-1
+  a = data1.T[0]
+  # normalize on [-1,1)
+  b = [ (e / 2**8)*2-1 for e in a ]
+  # FFT (amplitude (unknown units) over frequency (Hz))
+  c = fft(b)
 
-  ### data2 ####
+  #d-2
   a2 = data2.T[0]
-  b2 = [ (e / 2**8)*2 - 1 for e in a2 ]
+  # normalize on [-1,1)
+  b2 = [ (e / 2**8)*2-1 for e in a2 ]
+  # FFT (amplitude (unknown units) over frequency (Hz))
   c2 = fft(b2)
-  d2 = len(c2) / 2
 
   return (c, c2)
