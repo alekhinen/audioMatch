b466ea2b963c3a1b6cd9b895780042c16ae6288f
Nick Alekhine <nick.alekhine@gmail.com>
2014-10-08 13:40:04 -0400

    clean up. get ready for final version.

--- comparator.py
+++ comparator.py
@@ -13,8 +13,8 @@ def compareFiles(f1, f2):
 
   if ( len(f1) == len(f2) ):
     for i in range(0, len(f2) / 2):
-      abs_f1 = math.fabs(f1[i])
-      abs_f2 = math.fabs(f2[i])
+      abs_f1 = math.fabs(f1[i].real)
+      abs_f2 = math.fabs(f2[i].real)
 
       if ( math.fabs(abs_f1 - abs_f2) > THRESHOLD):
         result = 'NO MATCH'
