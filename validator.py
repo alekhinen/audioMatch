15ed5b6f689a1222e7e6b013f00e795066adfed7
Nick Alekhine <nick.alekhine@gmail.com>
2014-10-29 15:38:44 -0400

    validate wav file.

--- validator.py
+++ validator.py
@@ -36,13 +36,47 @@ def validateDirFiles( dirPath ):
 
   for subdir, dirs, files in os.walk( dirPath ):
     for f in files:
+      f = os.path.join( dirPath, f )
       result = result and validateFile( f )
 
   return result
 
 def validateFile( f ):
-  # TODO check if f is mp3 or wav and is actually an mp3 or wav.
+  isValid = False
 
+  # if file is a .wav
+  if f.endswith('.wav'):
+    #kind of file
+    kind = sndhdr.what( f )[0]
+    isValidKind = kind == 'wav'
+
+    #sample rate
+    sampRate = sndhdr.what( f )[1]
+    isValidSample = (sampRate == 44100
+      or sampRate == 11025
+      or sampRate == 22050
+      or sampRate == 48000)
+
+    #number of channels (mono or stereo)
+    numChan = sndhdr.what( f )[2]
+    isValidChan = numChan == 1 or numChan == 2
+
+    #bit rate
+    bitRate = sndhdr.what( f )[4]
+    isValidBitRate = bitRate == 8 or bitRate == 16
+
+    isValid = (isValidKind
+      and isValidSample
+      and isValidChan
+      and isValidBitRate)
+
+  # if file is a .mp3
+  elif f.endswith('.mp3'):
+    # TODO: parse this.
+    os.system('file %s' % f )
+    isValid = True
+
+  return isValid
 
 
 # -----------------------------------------------------------------------------
