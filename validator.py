ccf0b3d0d6ae77bda3458e1ecb2e468e515b07c5
Nick Alekhine <nick.alekhine@gmail.com>
2014-10-29 15:47:35 -0400

    finish up initial runthrough of parser, validator, and logger.

--- validator.py
+++ validator.py
@@ -72,38 +72,15 @@ def validateFile( f ):
 
   # if file is a .mp3
   elif f.endswith('.mp3'):
-    # TODO: parse this.
-    os.system('file %s' % f )
-    isValid = True
+    # TODO: read this
+    # https://stackoverflow.com/questions/3503879/assign-output-of-os-system-to-a-variable-and-prevent-it-from-being-displayed-on
+    fMeta = os.popen('file %s' % f ).read()
+    # check if it is 'MPEG'
+    isValid = 'MPEG' in fMeta
+    # check if it is 'layer III'
+    isValid = isValid and 'layer III' in fMeta
+    # check if it is 'v1'
+    isValid = isValid and 'v1' in fMeta
 
   return isValid
-
-
-# -----------------------------------------------------------------------------
-# OLD SHIT
-# #Validates the given file, in the directory signified by dirr
-# def Validate( core, f1, dirr ):
-#   #f1 is the file name as a string
-#   #dirr is 0 for the U_dir 1 for the A_Dir
-#   if dirr == 0:  #may need backslash safety
-#     full_dir = core['A_Dir'] + f1
-#   elif dirr == 1:
-#     full_dir = core['U_Dir'] + f1
-#   else:
-#     print "ERROR invalid core dir"
-
-#   if ( not (os.path.isfile( full_dir))):
-#     print "ERROR bad path name"
-
-#   form = sndhdr.what(full_dir)
-#   if form == 'wav':
-#     #TODO
-#   elif form == 'mp3':
-#     #TODO
-#   else:
-#     print "ERROR unsupported format"
-
-
-# def ValidateAll( core ):
-#   #TODO a extra edit here
 
