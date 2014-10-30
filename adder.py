a309614f0fe217588c881a123ff16c10503b5605
Michael Chadbourne <ziglish@gmail.com>
2014-10-30 16:39:32 -0400

    small tweaks to create and remove directories

--- adder.py
+++ adder.py
@@ -7,10 +7,12 @@ from os.path import isfile, join
 from processor import process
 from copyconvert import copyFile, convertFile
 from sndhdr import what
-
+from os import makedirs
 # Adds all the files in the user directory to the tmp directory
 # converts them and then adds them to the Database.
 def add ( core ) :
+  mkdirs("./tmp/User/")
+  mkdirs("./tmp/Ads/")
   # Generates the list of files in the user directory
   filesUser = []
   for i in listdir(core['U_Dir']):
