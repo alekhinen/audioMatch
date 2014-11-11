78a644cc27d7a95081d520b262cbf6a0fcffda88
Nick Alekhine <nick.alekhine@gmail.com>
2014-11-11 15:18:14 -0500

    add more unit tests to blackbox.

--- tests/blackbox/test_operationsCore.py
+++ tests/blackbox/test_operationsCore.py
@@ -27,6 +27,7 @@ class TestOperationsCore:
   args1 = ['i', 'am', 'stupid']
   args2 = ['this', 'is', 'bad']
   args3 = ['./dan', '-f', '../assets/D1/curieuse.mp3', '-d', '../assets/D2/']
+  args4 = ['./dan', '-d', '../assets/D1', '-d', '../assets/D2']
 
   def reset( self ):
     self.ocore1 = OperationsCore(1, 1)
@@ -36,6 +37,7 @@ class TestOperationsCore:
     self.args1 = ['i', 'am', 'stupid']
     self.args2 = ['this', 'is', 'bad']
     self.args3 = ['./dan', '-f', '../assets/D1/curieuse.mp3', '-d', '../assets/D2/']
+    self.args4 = ['./dan', '-d', '../assets/D1', '-d', '../assets/D2']
 
   @raises( SystemExit )
   def testSetArguments1( self ):
@@ -55,8 +57,43 @@ class TestOperationsCore:
     assert_equals( self.ocore1.getUsersDir(), '../assets/D1/curieuse.mp3' )
     assert_equals( self.ocore1.getAdsDir(), '../assets/D2/' )
 
-  def testConvertFiles( self ):
+  def testConvertFiles1( self ):
     self.reset()
     self.ocore1.setArguments( self.args3 )
     self.ocore1.convertFiles()
+    # in tmp/users
     assert_equals(os.path.exists('./tmp/users/curieuse.mp3'), True)
+    # in tmp/ads
+    assert_equals(os.path.exists('./tmp/ads/curieuse.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/janacek2.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/maynard2.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/rimsky2.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/sons.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/z04.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/z08.wav'), True)
+
+  def testConvertFiles2( self ):
+    self.reset()
+    self.ocore1.setArguments( self.args4 )
+    self.ocore1.convertFiles()
+    # in tmp/users
+    assert_equals(os.path.exists('./tmp/users/bad0616.wav'), True)
+    assert_equals(os.path.exists('./tmp/users/curieuse.mp3'), True)
+    assert_equals(os.path.exists('./tmp/users/curieuse2.wav'), True)
+    assert_equals(os.path.exists('./tmp/users/janacek.mp3'), True)
+    assert_equals(os.path.exists('./tmp/users/maynard.wav'), True)
+    assert_equals(os.path.exists('./tmp/users/rimsky.mp3'), True)
+    assert_equals(os.path.exists('./tmp/users/sons2.wav'), True)
+    assert_equals(os.path.exists('./tmp/users/Sor3508.mp3'), True)
+    assert_equals(os.path.exists('./tmp/users/WhoopeeTiYiYo.mp3'), True)
+    assert_equals(os.path.exists('./tmp/users/z03.wav'), True)
+    assert_equals(os.path.exists('./tmp/users/z07.wav'), True)
+    # in tmp/ads
+    assert_equals(os.path.exists('./tmp/ads/curieuse.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/janacek2.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/maynard2.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/rimsky2.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/sons.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/z04.wav'), True)
+    assert_equals(os.path.exists('./tmp/ads/z08.wav'), True)
+
