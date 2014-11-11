e278ce74c8b47f8875ab04255d71481fc86cd238
Nicholas A. Alekhine <alekhn@burninrubber.ccs.neu.edu>
2014-11-10 21:50:11 -0500

    get copyconvert working. add passing unit tests.

--- src/copyconvert.py
+++ src/copyconvert.py
@@ -5,8 +5,8 @@
 # -----------------------------------------------------------------------------
 # imports
 
-import os.path
-import os.remove
+from os import path
+from os import remove
 import subprocess
 
 # -----------------------------------------------------------------------------
@@ -20,26 +20,27 @@ import subprocess
 # @returns - ???
 def copyConvert( filepath, folder ):
   # break apart supplied filepath
-  basename = os.path.basename(filepath)
-  filename = os.path.splitext(basename)[0]
-  fileExtension = os.path.splitext(basename)[1]
+  basename = path.basename(filepath)
+  filename = path.splitext(basename)[0]
+  fileExtension = path.splitext(basename)[1]
   
   # create the future filepath
   futureFile = folder + filename + fileExtension
   ffMono = futureFile + '_mono.mp3'
   ffResampled = futureFile + '_resampled.mp3'
 
+  # TODO: absolute filepath to lame. unsure if this is the right thing to do.
   # initialize subprocess calls
   # convert original to mono
-  subProcess1 = ['../vendor/lame', '-a', filepath, ffMono 
+  subProcess1 = ['/course/cs4500f14/bin/lame', '-a', filepath, ffMono] 
   # convert mono to 8192 samples/sec
-  subProcess2 = ['../vendor/lame', '--resample', '8.192',  ffMono, ffResampled]
+  subProcess2 = ['/course/cs4500f14/bin/lame', '--resample', '8.192',  ffMono, ffResampled]
   # convert resampled to wav (maintaining original extension)
-  subProcess3 = ['../vendor/lame', '--decode', ffResampled, futureFile]
+  subProcess3 = ['/course/cs4500f14/bin/lame', '--decode', ffResampled, futureFile]
   subprocess.call( subProcess1 )
   subprocess.call( subProcess2 )
   subprocess.call( subProcess3 )
   
   # remove extraneous files
-  os.remove(ffMono)
-  os.remove(ffResampled)
+  remove(ffMono)
+  remove(ffResampled)
--- tests/glassbox/testCopyConvert.py
+++ tests/glassbox/testCopyConvert.py
@@ -1,4 +1,4 @@
-# AudioMatch Testing Suite
+#AudioMatch Testing Suite
 # @description glassbox tests for src/copyconvert
 # @author Nick Alekhine
 # @version 10-11-2014 (DD-MM-YYYY)
@@ -8,12 +8,12 @@
 # -----------------------------------------------------------------------------
 
 from nose.tools import *
-import os.makedirs
-import os.rmdir
+from os import makedirs
+from shutil import rmtree
 import os.path
 import sys
 sys.path.append('../../src')
-from copyconvert import copyConvert
+import copyconvert
 
 # -----------------------------------------------------------------------------
 # TESTS
@@ -22,22 +22,44 @@ from copyconvert import copyConvert
 # tests copyConvert() method
 class TestCopyConvert:
 
+  # TODO: unit tests on every single file in D1 & D2. (to check running time)
+
   # files
   f1 = '../assets/D1/bad0616.wav'
   f2 = '../assets/D2/curieuse.wav'
   f3 = '../assets/D1/rimsky.mp3'
 
   # destination directories
-  d1 = '../assets/tmp/D1'
-  d2 = '../assets/tmp/D2'
+  d1 = '../assets/tmp/D1/'
+  d2 = '../assets/tmp/D2/'
 
   # initialize destination directories
-  os.makedirs(d1)
-  os.makedirs(d2)
-  
+  def reset( self ):
+    self.implode() 
+    makedirs(self.d1)
+    makedirs(self.d2)
+ 
+  # destroys destination directories (if they exist)
+  def implode( self ):
+    if (os.path.exists(self.d1)):
+      rmtree(self.d1)
+    if (os.path.exists(self.d2)):
+      rmtree(self.d2)
+
   # testCopyConvert()
   # @description: tests the copyConvert() method from copyconvert
   def testEquals( self ):
-    copyConvert(f1, d1)
+    self.reset()
+    
+    # conversion 1
+    copyconvert.copyConvert(self.f1, self.d1)
     assert_equal(os.path.exists('../assets/tmp/D1/bad0616.wav'), True)
- 
+    # conversion 2
+    copyconvert.copyConvert(self.f2, self.d2)
+    assert_equal(os.path.exists('../assets/tmp/D2/curieuse.wav'), True)
+    # conversion 3
+    copyconvert.copyConvert(self.f3, self.d2)
+    assert_equal(os.path.exists('../assets/tmp/D2/rimsky.mp3'), True)
+    
+    self.implode()
+
