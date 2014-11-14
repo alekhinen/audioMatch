e5181f5df14f8bca4f723a21a93dcf2ceeffc311
Michael Chadbourne <ziglish@gmail.com>
2014-11-14 11:27:30 -0500

    played with constants, and added mag

--- src/helpers/postProcessor.py
+++ src/helpers/postProcessor.py
@@ -3,21 +3,30 @@
 
 import math
 
+def mag (cnum):
+  return math.sqrt(cnum.real**2 + cnum.imag**2)
+
 def computeHash( frag ):
-  boundaries = [30,150,500,2500]
-  weights    = [1, 1, 1, 1]
-  binValues  = [0 ,0, 0, 0]
+  #boundaries = [30,150,500,2500]
+  #weights    = [1, 1, 1, 1]
+  #binValues  = [0 ,0, 0, 0]
+#2.0
+#  boundaries = [28, 47, 84, 149, 233, 396, 745, 1050]
+  boundaries = [20, 40, 80, 160, 320, 640, 1280]
+  weights    = [1, 1, 1, 1, 1, 1, 1]
+  binValues  = [0, 0, 0, 0, 0, 0, 0]
+
 
   avalue = 0
   for dat in frag:
-    avalue = avalue + abs(dat)
-  avalue = float(1000/(avalue/len(frag)))
+    avalue = avalue + mag(dat)
+  avalue = float(100/(avalue/len(frag)))
 
   j = 0
   for i in range(len(frag)):
     if (i >= boundaries[j]): 
       j += 1
-    binValues[j] += int(abs(frag[i])*avalue)
+    binValues[j] += int(mag(frag[i])*avalue)
 
   result = 0
   weightInterval = 0
